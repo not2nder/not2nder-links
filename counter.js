f5735e59467ceccc1872c0445a48d53ef493db68
not2nder <130621173+not2nder@users.noreply.github.com>
2024-05-20 20:36:58 -0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,5 +1,5 @@
 export async function getSubs(element) {
-  console.log(JSON.stringify(process.env))
+  console.log(JSON.stringify(import.meta.env))
   const channelID = "UCYneqmkqlHRkbInADINEPVQ";
   const reqURL = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${process.env.VITE_API_KEY}`;
   const res = await fetch(reqURL);

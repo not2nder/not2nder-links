de6abb7a007c7648ffe52058475dbf81fbeefeb3
not2nder <brtiodark@gmail.com>
2024-08-01 18:11:25 -0300

    email

--- main.js
+++ main.js
@@ -16,42 +16,9 @@ document.querySelector('#app').innerHTML = `
             <a href="https://instagram.com/2nder.dev" target="_blank"><i class="fa-brands fa-instagram"></i></a>
             <a href="https://github.com/not2nder" target="_blank"><i class="fa-brands fa-github"></i></a>
         </div>
+            <p style="color:white">email: lacosteandxr@gmail.com</p>
     </div>
     <div class="buttons">
-        <!-- fist item on top -->
-        <div class="pinned">
-            <div class="top">
-                <h2>upcoming release!</h2>
-            </div>
-            <div class="mid">
-                <img src="https://distrokid.imgix.net/http%3A%2F%2Fgather.fandalism.com%2F7129118--1FD897F7-447F-4926-8834A40AF6366D40--0--17108614--ARTWORKNONAMES.jpg?fm=jpg&q=75&w=800&s=efeabc79ac0dc19eb964fb350b00df33">
-                <div>
-                    <h2>decline friend request</h2>
-                    <a target="_blank" href="https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro">not2nder</a>, <a target="_blank" href="https://open.spotify.com/artist/6wPviRJYfGannQEY6owpMm">gurowinter</a>
-                </div>
-                </div>
-            <div class="bottom">
-                <div>
-                    <h2 id="days"></h2>
-                    <p>Days</p>
-                </div>
-                :
-                <div>
-                    <h2 id="hours"></h2>
-                    <p>Hours</p>
-                </div>
-                :
-                <div>
-                    <h2 id="mins"></h2>
-                    <p>Minutes</p>
-                </div>
-                :
-                <div>
-                    <h2 id="secs"></h2>
-                    <p>Seconds</p>
-                </div>
-            </div>
-        </div>
         <div class="yt">
             <div class="bottom" onclick="window.open('https://youtube.com/@not2nder')">
                 <div class="left">
@@ -74,10 +41,18 @@ document.querySelector('#app').innerHTML = `
             </div>
         </div>
         <button class="primary" onclick="window.open('https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro')">
-            <a href="https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro" target="_blank"><i class="fa-brands fa-spotify"></i> Ｓｐｏｔｉｆｙ</a>
+            <a href="https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro" target="_blank"><i class="fa-brands fa-spotify"></i> Ｓｐｏｔｉｆｙ - not2nder</a>
+        
+        </button>
+                <button class="primary" onclick="window.open('https://music.apple.com/us/artist/not2nder/1735580732')">
+            <a href="https://music.apple.com/us/artist/not2nder/1735580732" target="_blank"><svg fill="var(--white)" height="18px" width="18px" viewBox="0 0 512 512"><path d="M503.319,5.939c-5.506-4.705-12.783-6.767-19.958-5.635L169.555,49.852c-12.04,1.901-20.909,12.28-20.909,24.47v99.097 v156.903H99.097C44.455,330.323,0,371.073,0,421.161C0,471.25,44.455,512,99.097,512c54.642,0,99.097-40.75,99.097-90.839v-66.065 V194.588l264.258-41.725v136.169h-49.548c-54.642,0-99.097,40.75-99.097,90.839s44.455,90.839,99.097,90.839 S512,429.959,512,379.871v-66.065V123.871V24.774C512,17.529,508.827,10.646,503.319,5.939z"></path></svg> 𝔸𝕡𝕡𝕝𝕖 𝕄𝕦𝕤𝕚𝕔 - 2NDER</a>
+        </button>
+        
+                <button class="primary" onclick="window.open('https://open.spotify.com/artist/6YoQ3aGaPzWtw6iUUXr5Og')">
+            <a href="https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro" target="_blank"><i class="fa-brands fa-spotify"></i> Ｓｐｏｔｉｆｙ - 2NDER</a>
         </button>
         <button class="primary" onclick="window.open('https://music.apple.com/us/artist/not2nder/1735580732')">
-            <a href="https://open.spotify.com/artist/4WB56sWCElzGrYSNcZgwro" target="_blank"><svg fill="var(--white)" height="18px" width="18px" viewBox="0 0 512 512"><path d="M503.319,5.939c-5.506-4.705-12.783-6.767-19.958-5.635L169.555,49.852c-12.04,1.901-20.909,12.28-20.909,24.47v99.097 v156.903H99.097C44.455,330.323,0,371.073,0,421.161C0,471.25,44.455,512,99.097,512c54.642,0,99.097-40.75,99.097-90.839v-66.065 V194.588l264.258-41.725v136.169h-49.548c-54.642,0-99.097,40.75-99.097,90.839s44.455,90.839,99.097,90.839 S512,429.959,512,379.871v-66.065V123.871V24.774C512,17.529,508.827,10.646,503.319,5.939z"></path></svg> 𝔸𝕡𝕡𝕝𝕖 𝕄𝕦𝕤𝕚𝕔</a>
+            <a href="https://music.apple.com/us/artist/not2nder/1735580732" target="_blank"><svg fill="var(--white)" height="18px" width="18px" viewBox="0 0 512 512"><path d="M503.319,5.939c-5.506-4.705-12.783-6.767-19.958-5.635L169.555,49.852c-12.04,1.901-20.909,12.28-20.909,24.47v99.097 v156.903H99.097C44.455,330.323,0,371.073,0,421.161C0,471.25,44.455,512,99.097,512c54.642,0,99.097-40.75,99.097-90.839v-66.065 V194.588l264.258-41.725v136.169h-49.548c-54.642,0-99.097,40.75-99.097,90.839s44.455,90.839,99.097,90.839 S512,429.959,512,379.871v-66.065V123.871V24.774C512,17.529,508.827,10.646,503.319,5.939z"></path></svg> 𝔸𝕡𝕡𝕝𝕖 𝕄𝕦𝕤𝕚𝕔 - not2nder</a>
         </button>
         <button class="primary" onclick="window.open('https://instagram.com/2nder.dev')">
             <a href="https://instagram.com/2nder.dev" target="_blank"><i class="fa-brands fa-instagram"></i> 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮</a>
